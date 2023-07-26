24463929463db25821eddee7aeaf93bf06ea2812
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-07-27 08:01:37 +1000

    Steps 11-12: Import useDispatch and setBoard into App; define dispatch variable, and dispatch the setBoard action in startGameHandler

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import './App.css';
 import { Score } from './features/score/Score.jsx';
 import { Board } from './features/board/Board.jsx';
 // Add import statements below
-
+import { useDispatch } from 'react-redux';
+import { setBoard } from './features/board/boardSlice';
 
 
 const App = () => {
   // Add dispatch variable below
-  
+  const dispatch = useDispatch()
 
   const startGameHandler = () => {
     // Add action dispatch below
-    
+    dispatch(setBoard())
   };
 
   const tryAgainHandler = () => {
